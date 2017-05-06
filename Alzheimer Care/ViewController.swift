1d21c694db4eba0094f43bc974cf14a957030868
Hugo Piauilino <hugos94@gmail.com>
2017-05-05 22:09:09 -0300

    Initial Commit

---
+++ Alzheimer Care/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alzheimer Care
+//
+//  Created by Hugo Santos Piauilino Neto on 05/05/17.
+//  Copyright © 2017 Hugo Santos Piauilino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
