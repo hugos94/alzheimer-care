711588103b47f13b5eab3595e2fb3aad395c9bfa
Vitor Cortez <vitoracortez+github@gmail.com>
2017-05-09 13:57:00 -0300

    Refactor RegisterViewController
    Add a helper method to create OK buttons

--- Alzheimer Care/RegisterViewController.swift
+++ Alzheimer Care/RegisterViewController.swift
@@ -17,35 +17,34 @@ class RegisterViewController: UIViewController {
   @IBOutlet weak var userNameTextField: UITextField!
   @IBOutlet weak var userNumberTextField: UITextField!
   
+  let context = DataManager.context
   // capturador / carregador de imagem
   let imagePicker = UIImagePickerController()
-  
-  let context = DataManager.context
   
   override func viewDidLoad() {
     super.viewDidLoad()
     self.hideKeyboardWhenTappedAround()
-    self.setupImagePicker()
+    
+    self.imagePicker.delegate = self
+    self.imagePicker.allowsEditing = false
   }
   
   // MARK: - Actions
   
   @IBAction func capture(_ sender: Any) {
     if UIImagePickerController.isSourceTypeAvailable(.camera) {
-      imagePicker.allowsEditing = false
-      imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+      imagePicker.sourceType = .camera
       imagePicker.cameraCaptureMode = .photo
       imagePicker.modalPresentationStyle = .fullScreen
       present(imagePicker, animated: true, completion: nil)
     } else {
       let alert = UIAlertController(title: "Sem câmera", message: "Desculpe, esse dispositivo não tem câmera", preferredStyle: .alert)
-      alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+      alert.addAction(OKButton())
       present(alert, animated: true, completion: nil)
     }
   }
   
   @IBAction func load(_ sender: Any) {
-    imagePicker.allowsEditing = false
     imagePicker.sourceType = .photoLibrary
     imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
     present(imagePicker, animated: true, completion: nil)
@@ -54,25 +53,23 @@ class RegisterViewController: UIViewController {
   @IBAction func registerButton(_ sender: Any) {
     guard let name = userNameTextField.text, let number = userNumberTextField.text else {
       let alert = UIAlertController(title: "Erro!", message: "Preencha todos os campos :D", preferredStyle: .actionSheet)
-      alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+      alert.addAction(OKButton())
       present(alert, animated: true, completion: nil)
       return
     }
-    
     UserDefaults.standard.set(number, forKey: "USER_NUMBER")
     UserDefaults.standard.set(name, forKey: "USER_NAME")
     
     dismiss(animated: true, completion: nil)
   }
+  
+  private func OKButton() -> UIAlertAction {
+    return UIAlertAction(title: "OK", style: .default, handler: nil)
+  }
 }
 
 extension RegisterViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-  func setupImagePicker() {
-    self.imagePicker.delegate = self
-  }
-  
   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-    picker.dismiss(animated: true, completion: nil)
     if let pickedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
       userPhotoImageView.contentMode = .scaleAspectFit
       userPhotoImageView.image = pickedImage
@@ -82,7 +79,7 @@ extension RegisterViewController: UIImagePickerControllerDelegate, UINavigationC
     } else {
       print("Failed to convert image for some reason")
     }
-    //dismiss(animated: true, completion: nil)
+    dismiss(animated: true, completion: nil)
   }
   
   func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
