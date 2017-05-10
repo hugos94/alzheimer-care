8faf951da0311fbf296ce9e4bf9fa32140bbbf47
Hugo Piauilino <hugos94@gmail.com>
2017-05-10 00:11:28 -0300

    Rafael updated add. Notifications on Physical Activities.

--- Alzheimer Care/PhysicalActivityTableViewController.swift
+++ Alzheimer Care/PhysicalActivityTableViewController.swift
@@ -60,25 +60,131 @@ class PhysicalActivityTableViewController: UITableViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        let content = UNMutableNotificationContent()
-        content.title = NSString.localizedUserNotificationString(forKey: "Wake up!", arguments: nil)
-        content.body = NSString.localizedUserNotificationString(forKey: "Rise and shine! It's morning time!", arguments: nil)
+        //registerForNotifications()
         
-        // Configure the trigger for a 7am wakeup.
-        var dateInfo = DateComponents()
-        dateInfo.hour = 22
-        dateInfo.minute = 18
-        let trigger = UNCalendarNotificationTrigger(dateMatching: dateInfo, repeats: false)
         
-        // Create the request object.
-        let request = UNNotificationRequest(identifier: "MorningAlarm", content: content, trigger: trigger)
         
-        // Schedule the request.
-        let center = UNUserNotificationCenter.current()
-        center.add(request) { (error : Error?) in
-            if let theError = error {
-                print(theError.localizedDescription)
+        scheduleNotificationTo10SecondsFromNow(repeating: false)
+        
+        /* let content = UNMutableNotificationContent()
+         content.title = NSString.localizedUserNotificationString(forKey: "Wake up!", arguments: nil)
+         content.body = NSString.localizedUserNotificationString(forKey: "Rise and shine! It's morning time!", arguments: nil)
+         
+         // Configure the trigger for a 7am wakeup.
+         var dateInfo = DateComponents()
+         dateInfo.hour = 22
+         dateInfo.minute = 18
+         let trigger = UNCalendarNotificationTrigger(dateMatching: dateInfo, repeats: false)
+         
+         // Create the request object.
+         let request = UNNotificationRequest(identifier: "MorningAlarm", content: content, trigger: trigger)
+         
+         // Schedule the request.
+         let center = UNUserNotificationCenter.current()
+         center.add(request) { (error : Error?) in
+         if let theError = error {
+         print(theError.localizedDeion)
+         }
+         } */
+        
+        /* let center = UNUserNotificationCenter.current()
+         
+         center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+         if granted {
+         print("Yay!")
+         } else {
+         print("D'oh")
+         }
+         }
+         
+         scheduleNotification() */
+        
+    }
+    
+    /* func scheduleNotification() {
+     let center = UNUserNotificationCenter.current()
+     
+     let content = UNMutableNotificationContent()
+     content.title = "Late wake up call"
+     content.body = "The early bird catches the worm, but the second mouse gets the cheese."
+     content.categoryIdentifier = "alarm"
+     content.userInfo = ["customData": "fizzbuzz"]
+     content.sound = UNNotificationSound.default()
+     
+     var dateComponents = DateComponents()
+     dateComponents.hour = 9
+     dateComponents.minute = 32
+     let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+     
+     //        var dateComponents = DateComponents()
+     //        dateComponents.hour = 9
+     //        dateComponents.minute = 23
+     //        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+     
+     let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+     center.add(request)
+     } */
+    
+    func registerForNotifications() {
+        // Defina o tipo de notificações que você quer permitir
+        let notificationTypes: UNAuthorizationOptions = [.sound, .alert, .badge]
+        
+        // Utilizamos o notification center para solicitar autorização
+        let notificationCenter = UNUserNotificationCenter.current()
+        
+        // Solicitamos autorização
+        notificationCenter.requestAuthorization(options: notificationTypes) {
+            (granted, error) in
+            if granted {
+                print("Autorização concedida :D")
+            } else {
+                print("Autorização negada :(")
             }
+        }
+    }
+    
+    func scheduleNotificationTo10SecondsFromNow(repeating: Bool) {
+        
+        // Defina o conteúdo da notificação
+        let content = UNMutableNotificationContent()
+        content.title = "Title"
+        content.subtitle = "Subtitle"
+        content.body = "Body body body body"
+        
+        // Definimos o intervalo de tempo para disparar a ação
+        var timeInterval = 10.0
+        if repeating {
+            // Se a notificação for se repetir o mínimo é de 60 segundos
+            timeInterval = 60.0
+        }
+        
+        // Defina o gatilho da notificação
+        // O gatilho pode ser de várias formas, uma delas é intervalo de tempo
+        let trigger = UNTimeIntervalNotificationTrigger(
+            timeInterval: timeInterval,
+            repeats: repeating
+        )
+        
+        // Crie um request de notificação com
+        //  - Identificação: identifica a notificação unicamente
+        //  - Conteúdo: define o conteúdo da notificação
+        //  - Gatilho: define quando a notificação será ativada
+        let request = UNNotificationRequest(
+            identifier: "10seconds.notification",
+            content: content,
+            trigger: trigger
+        )
+        
+        // Pegue a instância unica do notification center no app
+        let notificationCenter = UNUserNotificationCenter.current()
+        
+        
+        // Aqui só removemos as notificações anteriores
+        notificationCenter.removeAllPendingNotificationRequests()
+        
+        // Agora adicionamos nossa request de notificação no notification center
+        notificationCenter.add(request) { (error) in
+            print("Ops, temos um erro aqui! Veja: \(error)")
         }
     }
     
