e156d92c1840ef037fb94c5623e8c0af09b635ff
Hugo Piauilino <hugos94@gmail.com>
2017-05-11 11:38:31 -0300

    Refactored time line controller.

--- Alzheimer Care/TimelineTableViewController.swift
+++ Alzheimer Care/TimelineTableViewController.swift
@@ -20,6 +20,8 @@ class TimelineTableViewController: UITableViewController, AVAudioPlayerDelegate,
     
     func updateList() {
         listOfMemories = MemoryDAO.read()
+        
+        tableView.reloadData()
     }
     
     // MARK: - System Functions
@@ -37,19 +39,18 @@ class TimelineTableViewController: UITableViewController, AVAudioPlayerDelegate,
         
         updateList()
         
-        tableView.reloadData()
     }
-
+    
     // MARK: - Table View Data Source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return listOfMemories.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "memoryCell", for: indexPath)
@@ -59,17 +60,19 @@ class TimelineTableViewController: UITableViewController, AVAudioPlayerDelegate,
         if let memoryCell = cell as? MemoryTableViewCell {
             memoryCell.memoryNameLabel.text = memory.name
             memoryCell.memoryDateLabel.text = MemoryDAO.getFormattedData(date: memory.date as! Date)
+            memoryCell.memoryPlayButton.setImage(#imageLiteral(resourceName: "playRecord"), for: .normal)
             memoryCell.onButtonTapped = {
                 self.audioPlayer = try! AVAudioPlayer(contentsOf: URL(string: memory.url!)!)
                 self.audioPlayer.prepareToPlay()
                 self.audioPlayer.delegate = self
                 self.audioPlayer.play()
+                memoryCell.memoryPlayButton.setImage(#imageLiteral(resourceName: "playRecordFilled"), for: .normal)
             }
         }
         
         return cell
     }
-
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             if MemoryDAO.delete(listOfMemories[indexPath.row]){
@@ -115,11 +118,11 @@ class TimelineTableViewController: UITableViewController, AVAudioPlayerDelegate,
             AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
         ]
     }
-
+    
     // MARK: - Play Functions
     
     func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
-        print(flag)
+        updateList()
     }
     func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?){
         print(error.debugDescription)
@@ -129,7 +132,7 @@ class TimelineTableViewController: UITableViewController, AVAudioPlayerDelegate,
     }
     
     // MARK: - Navigation
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "addMemorySegue" {
             if let addNavigationController = segue.destination as? UINavigationController {
@@ -144,5 +147,5 @@ class TimelineTableViewController: UITableViewController, AVAudioPlayerDelegate,
     func userDidEnterInformation(memory: Memory) {
         listOfMemories.append(memory)
     }
-
+    
 }
